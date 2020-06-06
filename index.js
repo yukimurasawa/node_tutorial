11666a20dfee5884ba53ef3fe18d4cc11c7abb30
yukimurasawa <yuki.murasawa.0826@gmail.com>
2020-06-06 15:39:12 +0900

    server run

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-const Person = require("./person");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const person1 = new Person("yuki", 30);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
 
-person1.greeting();
+  if (req.url === "/api/users") {
+    const users = [
+      { name: "bob", age: 40 },
+      { name: "alice", age: 30 }
+    ];
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server runnning on port ${PORT}`));
